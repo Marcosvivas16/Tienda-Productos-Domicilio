1f07a629eec3166d77da5015b8d864a0d727ae44
Diego <diva.blas@gmail.com>
2025-03-15 11:59:54 +0100

    Update página de inicio para mostrar logo y título

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import tiendaLogo from './assets/logo.png'
 import './App.css'
 
 function App() {
@@ -8,15 +7,18 @@ function App() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+      </div> */}
+      <div>
+        <img src={tiendaLogo} className='logo' alt='Tienda logo' />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Tienda de Productos a Domicilio</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,8 +27,8 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="workingProgress">
+        Website on progress...
       </p>
     </>
   )
