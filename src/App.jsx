f9573aec5c43cae8d392b50bc993aff2d4028f38
Diego <diva.blas@gmail.com>
2025-03-16 14:10:45 +0100

    Add routing para páginas Home y Productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
-import { useState } from 'react'
-import tiendaLogo from './assets/logo.png'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Productos from "./pages/Productos";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      <div>
-        <img src={tiendaLogo} className='logo' alt='Tienda logo' />
-      </div>
-      <h1>Tienda de Productos a Domicilio</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="workingProgress">
-        Website on progress...
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/productos" element={<Productos />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
