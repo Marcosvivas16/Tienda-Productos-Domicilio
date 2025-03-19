767847d0622b6480ca875fde53c9b17e452dc76f
Vivas <businessvivas@gmail.com>
2025-03-19 10:40:04 +0100

    actualizacion de importaciones y cambios de estilo en el componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Inicio from "./home/Header";
+import Inicio from "./home/Inicio";
 import Productos from "./home/Productos";
 import Carrito from "./pages/Carrito";
 import Perfil from "./pages/Perfil";
