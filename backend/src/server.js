9690e180c3ccd62269f25333b308d822ae15c170
Diego Vallespín Blas <diva.blas@gmail.com>
2025-04-29 21:22:24 +0200

    Permitir conexión desde todas las direcciones

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,6 @@ import app from './app.js'
 
 const PORT = process.env.PORT ?? 1234
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`server listening on port http://localhost:${PORT}`)
 })
