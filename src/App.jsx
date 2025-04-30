d0f4f2288cc2dfa9de33ffaf8d4f2e5fcc1646bf
Hatim Bajji <hatimpeque@gmail.com>
2025-04-30 20:47:29 +0200

    Optimizacion App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Inicio from "./pages/Inicio";
 import Productos from "./pages/Productos";
+import DetalleProducto from "./pages/DetalleProducto";
 import Carrito from "./pages/Carrito";
+import Checkout from "./pages/Checkout";
 import Perfil from "./pages/Perfil";
+import Historial from "./pages/Historial";
 import Login from "./pages/Login";
 import Registro from "./pages/Registro";
-import Historial from "./pages/Historial";
-import DetalleProducto from "./pages/DetalleProducto";
-import Checkout from "./pages/Checkout";
+import ProtectedRoute from "./components/ProtectedRoute";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
-import ProtectedRoute from "./components/ProtectedRoute";
 import "./styles/App.css";
 
 function App() {
@@ -27,37 +27,33 @@ function App() {
                 <Route path="/" element={<Inicio />} />
                 <Route path="/productos" element={<Productos />} />
                 <Route path="/producto/:id" element={<DetalleProducto />} />
+                <Route path="/carrito" element={<Carrito />} />
                 <Route path="/login" element={<Login />} />
-                <Route 
-                  path="/carrito" 
-                  element={<Carrito />} 
-                />
-                <Route 
-                  path="/checkout" 
+                <Route path="/registro" element={<Registro />} />
+                <Route
+                  path="/checkout"
                   element={
                     <ProtectedRoute requireAuth={true}>
                       <Checkout />
                     </ProtectedRoute>
-                  } 
+                  }
                 />
-                <Route 
-                  path="/perfil" 
+                <Route
+                  path="/perfil"
                   element={
                     <ProtectedRoute requireAuth={true}>
                       <Perfil />
                     </ProtectedRoute>
-                  } 
+                  }
                 />
-                <Route 
-                  path="/historial" 
+                <Route
+                  path="/historial"
                   element={
                     <ProtectedRoute requireAuth={true}>
                       <Historial />
                     </ProtectedRoute>
-                  } 
+                  }
                 />
-                <Route path="/login" element={<Login />} />
-                <Route path="/registro" element={<Registro />} />
               </Routes>
             </main>
           </div>
